587ff92cebc8e166ca56b300528a50b04cb6443c
Albert Lima <137114275+Albert-Lima@users.noreply.github.com>
2024-09-05 05:55:11 -0300

    Atualizei
    adição de funcionalidade de Whatsapp

---
+++ public/script.js
@@ -0,0 +1,7 @@
+var buttonToWpp = document.querySelector("#RedirectToWpp")
+
+buttonToWpp.addEventListener("click", ()=>{
+    var messageWhatsapp = "https://wa.me/5516994539797"
+
+    window.open(messageWhatsapp, "_blank")
+})
